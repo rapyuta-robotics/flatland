131afc198a8d21978ec5f458e460c14136239d92
Jason Wang <jiashen.wang@rapyuta-robotics.com>
2018-10-26 16:55:07 +0900

    Fix formatting

--- flatland_server/include/flatland_server/layer.h
+++ flatland_server/include/flatland_server/layer.h
@@ -74,7 +74,7 @@ class Layer : public Entity {
     std::string path_;
     double resolution_;
     MapDescription() {}
-    MapDescription(const std::string& path, const double resolution)
+    MapDescription(const std::string &path, const double resolution)
         : path_(path), resolution_(resolution) {}
   };
 
