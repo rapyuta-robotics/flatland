9785a203498babb58a25c5a485fcc00517d963df
JiaShen Wang <jiashen.wang@rapyuta-robotics.com>
2019-01-30 12:07:48 +0900

    Clang

--- flatland_plugins/include/flatland_plugins/laser.h
+++ flatland_plugins/include/flatland_plugins/laser.h
@@ -167,7 +167,6 @@ class LaserCallback : public b2RayCastCallback {
   float ReportFixture(b2Fixture *fixture, const b2Vec2 &point,
                       const b2Vec2 &normal, float fraction) override;
 };
-
 };
 
 #endif
